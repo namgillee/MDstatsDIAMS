f1167486482798807929bed4b95fae0591e6aaf4
Namgil Lee <namgil.lee@kangwon.ac.kr>
2024-01-06 23:47:26 +0900

    fix basic functions

--- R/cov_diff.R
+++ R/cov_diff.R
@@ -1,19 +1,26 @@
-#' Covariance of diff's between two fragment ions
+#' Covariance of diff between each pair of fragment ions.
 #' 
-#' cov(d_{ij}, d_{i'j'})
+#' A diff for an ion i is defined by d_{i} = xbar_con1{i} - xbar_con2{i}, where
+#' xbar_con1 and xbar_con2 are the sample means for condition 1 and 2, 
+#' respectively. A covariance of diff between fragment ion i and j is 
+#'    cov(d_{i}, d_{j})
+#'    = cov(xbar_con1{i}, xbar_con1{j}) + cov(xbar_con2{i}, xbar_con2{j})
 #' 
-#' @param dat_con1,dat_con2  data matrix of normalized fragment ion peak area, 
+#' @param dat_con1,dat_con2  data matrices of normalized fragment ion peak area, 
 #'   each row is a replicate, each column is a fragment ion
-#' @return covariance matrix of fragment ions, ij.
+#' @param verbose  TRUE to print messages. Default is FALSE.
+#' @return covariance matrix of mean difference
 #' @examples 
-#'   dat_x = matrix(rnorm(9), 3, 3)
-#'   dat_y = matrix(rnorm(9), 3, 3)
+#'   dat_x = matrix(rnorm(12), 4, 3)
+#'   dat_y = matrix(rnorm(12), 4, 3)
 #'   cov_diff(dat_x, dat_y)
-cov_diff <- function(dat_con1, dat_con2) {
+cov_diff <- function(dat_con1, dat_con2, verbose = FALSE) {
     # check number of fragment ions
     if (ncol(dat_con1) != ncol(dat_con2)) {
+      if (verbose) {
         print("Number of columns in dat_con1 and dat_con2 must be same.")
-        return(matrix())
+      }
+      return(matrix())
     }
   
     # compute covariance matrix
@@ -21,16 +28,14 @@ cov_diff <- function(dat_con1, dat_con2) {
     result <- matrix(NA, num_frag, num_frag)
     for (i in 1:num_frag) {
         for (j in i:num_frag) {
-            cov_x <- var(dat_con1[, i], dat_con1[, j], na.rm = TRUE)
-            cov_y <- var(dat_con2[, i], dat_con2[, j], na.rm = TRUE)
-            if (is.na(cov_x) | is.na(cov_y)) {
-                num_x <- num_y <- 1
-            } else {
-                num_x <- sum((!is.na(dat_con1[, i])) & (!is.na(dat_con1[, j])))
-                num_y <- sum((!is.na(dat_con2[, i])) & (!is.na(dat_con2[, j])))
+            cov_con1 <- var(dat_con1[, i], dat_con1[, j], na.rm = TRUE)
+            num_con1 <- sum((!is.na(dat_con1[, i])) & (!is.na(dat_con1[, j])))
+            cov_con2 <- var(dat_con2[, i], dat_con2[, j], na.rm = TRUE)
+            num_con2 <- sum((!is.na(dat_con2[, i])) & (!is.na(dat_con2[, j])))
+            if ((!is.na(cov_con1)) && (!is.na(cov_con2))) {
+                result[i, j] <- result[j, i] <- 
+                  cov_con1 / num_con1 + cov_con2 / num_con2
             }
-            
-            result[i, j] <- result[j, i] <- cov_x / num_x + cov_y / num_y
         }
     }
     return(result)
--- R/paired_t_test.R
+++ R/paired_t_test.R
@@ -1,9 +1,11 @@
 #' Paired t-test for modified peptide using fragment ion peak area
 #' 
 #' @param quantity1,quantity2  numeric vectors of normalized fragment peak area
+#' @param conf.level  Confidence level. Default is 0.95
+#' @param verbose  TRUE to print messages. Default is FALSE.
 #' @examples 
 #' paired_t_test(rnorm(10), rnorm(10))
-paired_t_test <- function(quantity1, quantity2, var.equal = TRUE, 
+paired_t_test <- function(quantity1, quantity2,
                           conf.level = 0.95, verbose = FALSE) {
     # check input length
     if (length(quantity1) != length(quantity2)) {
@@ -21,7 +23,7 @@ paired_t_test <- function(quantity1, quantity2, var.equal = TRUE,
     }
     
     # perform paired t-test
-    ttest_out <- t.test(quantity1[!is_na], quantity2[!is_na], var.equal=var.equal, paired=TRUE,
+    ttest_out <- t.test(quantity1[!is_na], quantity2[!is_na], paired=TRUE,
                         conf.level=conf.level)
     result <- list(statsitic = ttest_out$statistic, df=ttest_out$parameter, 
                    p.value=ttest_out$p.value, estimate=ttest_out$estimate)
